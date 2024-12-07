bf4eae1b34d4961693feca80ac759d00bcd32b75
Ubaidmalik9567 <Ubaidmalik9567@yahoo.com>
2024-12-07 19:55:13 +0500

    Add .env file to Secure API keys

--- Backend/api_token.py
+++ Backend/api_token.py
@@ -13,16 +13,3 @@ class LargeLanguageModel():
         # Fetch the OpenAI GPT API key from environment variables
         gpt_api_key = os.getenv('GPT_API_KEY')
         return gpt_api_key
-
-
-# class LargeLanguageModel():
-#     def __init__(self):
-#         pass
-    
-#     def get_Key(self):
-#         __api_key = "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"
-#         return __api_key
-    
-#     def get_gpt_key(self):
-#         __api_key = "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"
-#         return __api_key
